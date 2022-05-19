08024aaf2408c4997784d7995ef1e88b9c4a6c76
Dave Hrdlicka <dave.hrdlicka@gmail.com>
2022-05-19 14:43:33 +0200

    Fix localhost network handling

--- deployments/getContractAddress.ts
+++ deployments/getContractAddress.ts
@@ -2,9 +2,23 @@ import fs from 'fs';
 
 import { ethers } from 'ethers';
 
+const getNetworkName = (network: ethers.providers.Networkish) => {
+  if (network === 'localhost') {
+    return network;
+  }
+
+  const standardName = ethers.providers.getNetwork(network)?.name;
+
+  if (standardName === 'unknown') {
+    return 'localhost';
+  }
+
+  return standardName ?? network;
+}
+
 const getContractAddress = (contractTag: string, network: ethers.providers.Networkish) => {
   try {
-    const networkName = ethers.providers.getNetwork(network)?.name ?? network;
+    const networkName = getNetworkName(network);
     const contractJSON = fs
       .readFileSync(
         `./contracts/deployments/${networkName}/${contractTag}.json`
