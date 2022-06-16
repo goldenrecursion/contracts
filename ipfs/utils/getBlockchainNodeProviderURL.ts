2e08a3e9230e88ac55d02de02d9c078e12c54d16
Constantin Musca <constantin.musca@gmail.com>
2022-06-16 13:41:18 -0700

    getBlockchainNodeProviderURL.ts: update rinkeby alchemy url to start over free tier
    Signed-off-by: Constantin Musca <constantin.musca@gmail.com>

--- ipfs/utils/getBlockchainNodeProviderURL.ts
+++ ipfs/utils/getBlockchainNodeProviderURL.ts
@@ -6,7 +6,7 @@ const getBlockchainNodeProviderURL = (network: ethers.providers.Networkish) => {
     case 'localhost':
       return 'http://127.0.0.1:8545';
     case 'rinkeby':
-      return 'https://eth-rinkeby.alchemyapi.io/v2/wJsSSKTeAmqF2G5D56g00CX8fzwumcu0';
+      return 'https://eth-rinkeby.alchemyapi.io/v2/Qi577B6D3wleEiJwHyoh9a2sj8vchoIr';
     case 'mainnet':
       return 'https://eth-mainnet.alchemyapi.io/v2/O4mYQs1L1DOma_ri_4rdGPjcDg8uRey9';
   }
