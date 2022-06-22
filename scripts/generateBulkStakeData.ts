170fc669c9454c00fc9b5b833a72d397468456c1
Mihail Gutan <gutan.mike@gmail.com>
2022-06-22 12:59:34 -0700

    clean

--- scripts/generateBulkStakeData.ts
+++ scripts/generateBulkStakeData.ts
@@ -1,6 +1,5 @@
-import { ethers } from 'hardhat'
-
-// import goldenTokenAbi from '../abis/GoldenTokenGoerli.json'
+import { ethers, deployments, getNamedAccounts } from 'hardhat'
+import { INITIAL_SUPPLY } from '../deploy/GoldenToken';
 
 const toStake: { addr: string, amount: string }[] = [
   {
@@ -8,30 +7,34 @@ const toStake: { addr: string, amount: string }[] = [
     amount: '10000000000000000000'
   }
 ]
-// const GOLDEN_TOKEN_GOERLI = '0x6B9a039f98eB5B613Bd1783AE728Bd04789ab5B8' //Prod Goldeb
-const GOLDEN_TOKEN_LOCAL = '0xc5a5C42992dECbae36851359345FE25997F5C42d'
-
 
 async function main() {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
 
-  // const provider = ethers.providers.getDefaultProvider()
-  // const address = GOLDEN_TOKEN_LOCAL
-  const Token = await ethers.getContractFactory("GoldenToken")
-  const token = await Token.attach(GOLDEN_TOKEN_LOCAL)
-
-  // const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider)
-
-  // const contract = new ethers.Contract(address, goldenTokenAbi, wallet)
+  await deploy('GoldenToken', {
+    log: true,
+    from: deployer,
+    proxy: {
+      proxyContract: 'OpenZeppelinTransparentProxy',
+      execute: {
+        methodName: 'initialize',
+        args: [INITIAL_SUPPLY],
+      }
+    }
+  });
+  const goldenToken = (await ethers.getContract('GoldenToken')).connect(
+    await ethers.getSigner(deployer)
+  );
 
   let totalAmount = BigInt('10000000000000000000')
 
-  const tx = await token.bulkStake(toStake, totalAmount)
+  const tx = await goldenToken.bulkStake(toStake, totalAmount)
 
   console.log('DONE', tx.data)
-  console.log('This should have stake', (await token.stakeOf('0x6B9a039f98eB5B613Bd1783AE728Bd04789ab5B8')).toString())
-  console.log('This should NOT have stake', (await token.stakeOf('0xc5a5C42992dECbae36851359345FE25997F5C42d')).toString())
+  console.log('This should have stake', (await goldenToken.stakeOf('0x6B9a039f98eB5B613Bd1783AE728Bd04789ab5B8')).toString())
+  console.log('This should NOT have stake', (await goldenToken.stakeOf('0xc5a5C42992dECbae36851359345FE25997F5C42d')).toString())
 
 }
-
 
 main()
