0420e035628cd629a77e32b9b97fe3ed6f81ff54
Emory Petermann <emory@onlyfor.us>
2022-08-24 10:06:53 -0500

    write file in prettier compatible format (#118)

--- ipfs/data/preloadPredicates.ts
+++ ipfs/data/preloadPredicates.ts
@@ -2,36 +2,22 @@ import fs from 'fs';
 
 import { UUIDToBytes16 } from '../utils/bytes16UUID';
 import { cidToBytes32 } from '../utils/bytes32IPFSHash';
-import { addToIPFS, IPFSPredicatePayload } from '../IPFSapi';
+import { addToIPFS } from '../IPFSapi';
 
 import predicates from './predicates.json';
 
-const preloadData = async (data: IPFSPredicatePayload[]) => {
-  const cids = await Promise.all(
-    data.map(async (item) => {
-      const cid = await addToIPFS(item);
-
-      return cid;
-    })
-  );
-
-  return cids;
-};
-
-const preload = async () => {
-  console.log('ADDING TO IPFS...');
-  const predicateCIDS = await preloadData(predicates);
-  console.log('ADD TO IPFS COMPLETED');
-  const data = predicates.map((predicate, i) => [
-    UUIDToBytes16(predicate.id),
-    cidToBytes32(predicateCIDS[i]!),
-  ]);
-  console.log('UPDATING goldenSchemaPredicates.json');
-  fs.writeFileSync(
-    __dirname + '/../../contracts/GoldenSchemaPredicates.json',
-    JSON.stringify(data, null, 2)
-  );
-  console.log('ALL DONE');
-};
-
-preload();
+Promise.all(predicates.map(addToIPFS))
+  .then((predicateCIDS) => {
+    return fs.writeFileSync(
+      __dirname + '/../../contracts/GoldenSchemaPredicates.json',
+      JSON.stringify(
+        predicates.map((predicate, i) => [
+          UUIDToBytes16(predicate.id),
+          cidToBytes32(predicateCIDS[i]),
+        ]),
+        null,
+        2
+      ) + '\n'
+    );
+  })
+  .catch(console.error);
