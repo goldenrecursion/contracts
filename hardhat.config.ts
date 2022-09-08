a42b678b692c10df96f286ca762757c0102b9528
Emory Petermann <emory@onlyfor.us>
2022-09-08 19:49:52 -0400

    update typechain v2->v3

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,13 +2,15 @@
 import * as dotenv from 'dotenv';
 
 import { HardhatUserConfig } from 'hardhat/config';
+import '@typechain/hardhat';
+
+import '@nomiclabs/hardhat-ethers';
 import '@nomiclabs/hardhat-etherscan';
 import '@nomiclabs/hardhat-waffle';
-import '@typechain/hardhat';
+
 import 'hardhat-gas-reporter';
 import 'solidity-coverage';
 import 'hardhat-deploy';
-import '@nomiclabs/hardhat-ethers';
 import '@openzeppelin/test-helpers';
 import '@openzeppelin/hardhat-upgrades';
 
