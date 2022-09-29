4299c867edfccd2913e8ddbe1e0c43bb02640ec7
Mihail Gutan <gutan.mike@gmail.com>
2022-09-29 15:56:54 -0700

    fix ts

--- scripts/generateWallets.ts
+++
@@ -1,8 +0,0 @@
-import { ethers, getNamedAccounts } from 'hardhat';
-
-async function main() {
-  console.log('process.argv', process.argv);
-  const wallet = ethers.Wallet.createRandom();
-}
-
-main();
