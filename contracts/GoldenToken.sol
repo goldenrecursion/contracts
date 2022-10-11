393cab21fb3b40d9bff4c8b839a2febbc5c8a55f
Gutyn <gutan.mike@gmail.com>
2022-10-11 11:16:00 -0700

    Script to fund multiple wallets from one wallet (#171)
    * Script to fund multiple wallets from one wallet (#171)

--- contracts/GoldenToken.sol
+++ contracts/GoldenToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.16;
 
 import '@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol';
--- contracts/libraries/Bytes16Set.sol
+++ contracts/libraries/Bytes16Set.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Unlicensed
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.16;
 
 // Inspired by: https://github.com/rob-Hitchens/SetTypes
 
