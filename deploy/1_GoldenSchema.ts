489d460344a10dd1050d4fb7a49ff50bd8d7634c
Constantin Musca <constantin.musca@gmail.com>
2022-11-11 21:47:08 +0200

    Make GoldenSchema contract upgradable (#220)
    * Make GoldenSchema upgradable
    
    Signed-off-by: Constantin Musca <constantin.musca@gmail.com>
    
    * Deploy GoldenSchemaUpgradable and GoldenSchemaGovernor
    
    Signed-off-by: Constantin Musca <constantin.musca@gmail.com>
    
    Signed-off-by: Constantin Musca <constantin.musca@gmail.com>

--- deploy/1_GoldenSchema.ts
+++ deploy/1_GoldenSchema.ts
@@ -2,18 +2,33 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
 import initialPredicates from '../contracts/GoldenSchemaPredicates.json';
 import initialEntityTypes from '../contracts/GoldenSchemaEntityTypes.json';
+import { deployerAddress } from '../hardhat.config';
 
 const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts } = hre;
+  const { deployments, getNamedAccounts, network } = hre;
+  const { deploy, catchUnknownSigner } = deployments;
 
   const { deployer } = await getNamedAccounts();
+  const dev = ['hardhat', 'localhost'].includes(network.name);
 
-  await deployments.deploy('GoldenSchema', {
-    from: deployer,
-    skipIfAlreadyDeployed: true,
-    args: [initialPredicates, initialEntityTypes],
-    log: true,
-  });
+  const contractName = 'GoldenSchema';
+  const depl = dev ? deployer : deployerAddress;
+  await catchUnknownSigner(
+    deploy(contractName, {
+      log: true,
+      from: depl,
+      proxy: {
+        owner: depl,
+        proxyContract: 'OpenZeppelinTransparentProxy',
+        execute: {
+          init: {
+            methodName: 'initialize',
+            args: [initialPredicates, initialEntityTypes],
+          },
+        },
+      },
+    })
+  );
 };
 
 deploy.id = 'deploy_golden_schema';
