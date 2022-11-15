97cecfe5f6d5989dc71cd91e3927d924af70c16b
Jed Christiansen <jedchristiansen@users.noreply.github.com>
2022-11-15 11:02:09 -0800

    Migrating ~120 predicates and ~10 entity types (#224)
    * Creating shell for MDTs for Batch2 entity types
    
    * Adding Batch2 entity types to is_a ENUM
    
    * Acquisition MDTs
    
    name AND acquirer AND acquired company
    
    * Add Funding Round MDTs
    
    name AND funded company AND funding type AND (funding round date OR date announced)
    
    * Add SBIR/STTR Award MDTs
    
    name AND award type AND award phase AND government agency AND contract number
    
    * Add Investment Fund MDTs
    
    name AND investment fund type AND parent investment fund AND total offering amount
    
    * Add Clinical Study MDTs
    
    name AND NCT number AND study type AND clinical trial start date
    
    * Add Educational Institute MDTs
    
    name AND website AND founded date
    
    * Add Patent MDTs
    
    name & patent jurisdiction & (patent number OR patent publication code) & (date of patent OR patent publication date) & date filed & patent application number
    
    * Add Legislation MDTs
    
    name AND jurisdiction AND date signed
    
    * Add Country MDTs
    
    name AND geonames ID
    
    * Edit Creative Work MDTs
    
    name (only)
    
    * Add Strategic Partnership MDTs
    
    name AND announcement URL AND partner organizations
    
    * Add Location to Company MDTs
    
    * Adding ~120 additional predicates
    
    * Fix predicates/entity types added by Jed
    
    Signed-off-by: Constantin Musca <constantin.musca@gmail.com>
    
    Signed-off-by: Constantin Musca <constantin.musca@gmail.com>
    Co-authored-by: Constantin Musca <constantin.musca@gmail.com>

--- ipfs/data/entityTypes.json.ts
+++ ipfs/data/entityTypes.json.ts
@@ -36,6 +36,9 @@ const json: readonly IPFSEntityTypeBody[] = [
           {
             predicate_id: '0a87e996-34b4-46ba-909a-70ab67b1f811',
           },
+          {
+            predicate_id: 'fd4de165-da27-4261-b045-a8aca9baf99b',
+          },
         ],
       },
     ],
@@ -121,6 +124,347 @@ const json: readonly IPFSEntityTypeBody[] = [
   {
     id: '40548ec9-55e8-4b7e-95e3-c5d9a4590235',
     name: 'Location',
+    mdt_and_rules: [
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: 'a27218b8-6a4d-47bb-95b6-5a55334fac1c',
+          },
+        ],
+      },
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: '866bb4b6-6b3a-4b42-b16a-c6b1f2ec5a59',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 'f55f803b-a98c-49f0-a748-0ab2b5132297',
+    name: 'SBIR/STTR Awards',
+    mdt_and_rules: [
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: 'a27218b8-6a4d-47bb-95b6-5a55334fac1c',
+          },
+        ],
+      },
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: 'b473cfda-b7e4-420f-b8f7-88031621f971',
+          },
+        ],
+      },
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: 'e175b0b0-fc8b-412c-9270-52158f7d7f37',
+          },
+        ],
+      },
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: 'dfd19088-36ce-4219-a828-428ef38a303c',
+          },
+        ],
+      },
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: '249f906a-855c-4eb7-b2fb-d5088f684010',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 'c93f9f81-cbfa-47b0-aae9-0e10d97b53f6',
+    name: 'Funding Round',
+    mdt_and_rules: [
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: 'a27218b8-6a4d-47bb-95b6-5a55334fac1c',
+          },
+        ],
+      },
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: '8fa38ac4-b0c5-4f46-a273-a2b6e03afae2',
+          },
+        ],
+      },
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: '7901939d-ccc3-4c51-8636-2078ad9e3dfd',
+          },
+        ],
+      },
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: '7535e456-84a6-4c18-943c-59ba885ea74a',
+          },
+          {
+            predicate_id: 'dff8d031-1b22-4359-a9a3-30e7fb5153ec',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: '2b9258df-ecd1-4394-93f5-868934fd4e8a',
+    name: 'Investment Fund',
+    mdt_and_rules: [
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: 'a27218b8-6a4d-47bb-95b6-5a55334fac1c',
+          },
+        ],
+      },
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: '4ae3d52d-bb3a-4f67-94f4-690d626afb40',
+          },
+        ],
+      },
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: '6a8142a3-e66d-4198-aa4d-e8973e876833',
+          },
+        ],
+      },
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: '933b82c0-6ac7-446d-ad94-cee5ca44ccac',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: '2fada829-b268-4542-9dc2-8740248e975d',
+    name: 'Clinical Study',
+    mdt_and_rules: [
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: 'a27218b8-6a4d-47bb-95b6-5a55334fac1c',
+          },
+        ],
+      },
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: '2f65a0b3-8f11-4be8-9c37-7f03a7e5e744',
+          },
+        ],
+      },
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: 'bde884df-85bd-4d7f-aa59-c3369c3910f2',
+          },
+        ],
+      },
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: '6c7c41d6-ca87-4ee2-8ab9-9ca03762cbb2',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: '193af481-d970-4a28-b2aa-1d39f6f42c03',
+    name: 'Educational Institute',
+    mdt_and_rules: [
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: 'a27218b8-6a4d-47bb-95b6-5a55334fac1c',
+          },
+        ],
+      },
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: '42cb158b-e836-45ed-9b56-034668b8f05a',
+          },
+        ],
+      },
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: 'fa1a5ac7-480c-4e44-a545-b0f3dd9d24bf',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: '6b3b4f73-1548-4a54-96c7-f722c544df48',
+    name: 'Patent',
+    mdt_and_rules: [
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: 'a27218b8-6a4d-47bb-95b6-5a55334fac1c',
+          },
+        ],
+      },
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: 'ec741e8c-5589-4a64-b9e1-4fd41fd2b05f',
+          },
+        ],
+      },
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: '9aaba6ce-31a5-4bc0-8f26-390d354e96cd',
+          },
+          {
+            predicate_id: '1f50b09a-91ad-46d3-91e2-03c551af63f1',
+          },
+        ],
+      },
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: 'ecdb4b42-e12a-4084-8503-b1aeda5310ee',
+          },
+          {
+            predicate_id: '4a1f9158-90b7-47d3-a570-d264a2ac3c71',
+          },
+        ],
+      },
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: '6fe6e555-ee51-4a28-aae4-1ab84fdf291e',
+          },
+        ],
+      },
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: 'b09db03a-29d4-42c3-a94d-76c6c7d32c58',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: '6d29d6b2-0256-4ae0-9cc4-bdc556ac7d8d',
+    name: 'Acquisition',
+    mdt_and_rules: [
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: 'a27218b8-6a4d-47bb-95b6-5a55334fac1c',
+          },
+        ],
+      },
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: 'f8337728-2cce-42b1-8fa0-3c4ac5d38eb7',
+          },
+        ],
+      },
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: '13596c53-cc26-4750-b337-f8a85577aa7a',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: '273f6736-1c67-4b46-ab23-e9348b466b41',
+    name: 'Legislation',
+    mdt_and_rules: [
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: 'a27218b8-6a4d-47bb-95b6-5a55334fac1c',
+          },
+        ],
+      },
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: 'adaf84bc-35b4-41e2-a466-1157f5b932af',
+          },
+        ],
+      },
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: '4b8d2824-8095-42c0-9502-26fa58811876',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: '104ac3a1-bf25-49be-9f0a-1b46104e06eb',
+    name: 'Strategic Partnership',
+    mdt_and_rules: [
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: 'a27218b8-6a4d-47bb-95b6-5a55334fac1c',
+          },
+        ],
+      },
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: 'f89d3138-f12f-4f1f-b1ed-652202d78b5b',
+          },
+        ],
+      },
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: 'b714a484-704d-4dc7-a71f-859c6583326e',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: '163946c5-465f-47dd-8907-f8f2ca4b29da',
+    name: 'Creative Work',
+    mdt_and_rules: [
+      {
+        mdt_or_rules: [
+          {
+            predicate_id: 'a27218b8-6a4d-47bb-95b6-5a55334fac1c',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 'ca399d7a-e559-4062-9570-1b69c667ca10',
+    name: 'Country',
     mdt_and_rules: [
       {
         mdt_or_rules: [
