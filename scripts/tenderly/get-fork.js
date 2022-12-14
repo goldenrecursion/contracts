1d17f6c9edc1a331add3d339abd2c074e4bcc5cb
hopisaurus <hopisaurus@gmail.com>
2022-12-14 15:49:39 -0500

    CI: Feature/delete-tenderly-fork (#235)
    * ci: add get-fork logic
    
    * ci: add delete fork workflow
    
    * ci: cleanup env usage
    
    * CI: Update scripts/tenderly/get-fork.js
    
    Co-authored-by: Elvis <43846394+Elvis339@users.noreply.github.com>
    
    * feat: improve get-fork
    
    * lint
    
    * feat: use fork name instead of non-existing fork_id
    
    Co-authored-by: Elvis <43846394+Elvis339@users.noreply.github.com>
    Co-authored-by: Elvis S <elvissabanovic3@gmail.com>

---
+++ scripts/tenderly/get-fork.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const axios = require('axios');
+const { TENDERLY_PROJECT, TENDERLY_ACCESS_KEY, FORK_NAME } = process.env;
+
+if (!FORK_NAME) {
+  throw new Error(`Missing env: FORK_NAME`);
+}
+
+// return forks from project
+const TENDERLY_FORKS = `https://api.tenderly.co/api/v2/project/${TENDERLY_PROJECT}/forks`;
+
+axios
+  .get(TENDERLY_FORKS, {
+    headers: {
+      'X-Access-Key': TENDERLY_ACCESS_KEY,
+    },
+  })
+  .then((res) => {
+    if (!Array.isArray(res.data)) {
+      throw new Error(`Tenderly error`);
+    }
+
+    const fork = res.data.find(({ name }) => name === FORK_NAME);
+    console.log({
+      fork,
+      FORK_NAME,
+    });
+
+    if (!fork) {
+      throw new Error(`Fork not found`);
+    }
+
+    return process.stdout.write(fork.id);
+  })
+  .catch(process.stderr.write);
