b39337e53163a24c604a6731a9739369582527a4
Mihail Gutan <gutan.mike@gmail.com>
2022-12-15 14:48:30 -0800

    fix tests

--- deploy/1_GoldenSchema.ts
+++ deploy/1_GoldenSchema.ts
@@ -23,7 +23,7 @@ const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
         execute: {
           init: {
             methodName: 'initialize',
-            args: [[], []],
+            args: [initialPredicates, initialEntityTypes],
           },
         },
       },
