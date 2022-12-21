b12e5251892ea3b8e11a6556e5dbac42717864c9
Elvis S <elvissabanovic3@gmail.com>
2022-12-21 18:29:25 +0100

    feat: get accounts allowed to interact with the fork and add 100 forkedETH as starting balance

--- scripts/tenderly/deploy.ts
+++ scripts/tenderly/deploy.ts
@@ -49,7 +49,7 @@ async function main() {
   await golden.initialize(INITIAL_SUPPLY);
 
   console.log({
-    ...accounts,
+    accounts: { ...accounts },
     deployed_by: await Golden.signer.getAddress(),
   });
 }
